5ea33537e8a9ea5e4b542c1342d4be7020ce785a
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2025-03-14 23:56:06 +0500

    Add: Content security policy

--- run.py
+++ run.py
@@ -29,7 +29,25 @@ def inject_primary_color():
         "AUTHENTICATED_ROLE": AUTHENTICATED_ROLE,
         "_words": words.get(request.cookies.get('lang', 'en'), words.get('en')),
         "_host": os.getenv('HOST'),
+        "_nonce": f'{g.nonce}'
     }
+
+
+@app.before_request
+def generate_nonce():
+    if not hasattr(g, "nonce"):
+        g.nonce = os.urandom(16).hex()
+
+
+@app.after_request
+def add_security_headers(response):
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers['Content-Security-Policy'] = f"default-src 'self'; " \
+                                                  f"style-src 'self' 'sha256-/pKNHmGRCORjIYUPE3fRB7lJIjWnuhyErIcu5EbSucM=' https://cdnjs.cloudflare.com;" \
+                                                  f"script-src 'self' https://cdn.jsdelivr.net;" \
+                                                  f"frame-ancestors 'none';" \
+                                                  f"form-action 'self';"
+    return response
 
 
 if __name__ == "__main__":
