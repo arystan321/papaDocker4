02181ddf4107d3326b14fc8698a0d4b83f87d124
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2025-03-19 01:47:14 +0500

    Fix: user data authentication public info error to default values

--- app/services/keycloak.py
+++ app/services/keycloak.py
@@ -124,7 +124,7 @@ class KeyCloak(AuthenticationService):
 
         response = requests.get(url, headers=headers, verify=False)
         user_data = response.json()
-        if not user_data.get:
+        if not user_data or not isinstance(user_data[0], dict):
             user_data = {}
 
         return {
